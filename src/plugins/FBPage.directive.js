e0344a13165f2580bd3f70e0511592a736e91cc5
praveenkumar-outlook <praveen.ece@outlook.com>
2018-08-30 15:49:04 +0530

    Plugin : FB Page directive

--- src/plugins/FBPage.directive.js
+++ src/plugins/FBPage.directive.js
@@ -1,12 +1,73 @@
 import angular from 'angular';
+import _ from 'underscore';
 
 export default angular.module('fbPagePlugin', [])
-  .directive('fbPage', () => {
+  .directive('fbPage', ['$window', ($window) => {
     return {
       restrict: 'E',
       scope: {
-        name: '='
+        adaptContainerWidth: '@',
+        appId: '@',
+        height: '@',
+        hideCover: '@',
+        hideCta: '@',
+        href: '@',
+        locale: '@',
+        showFacepile: '@',
+        smallHeader: '@',
+        tabs: '@',
+        version: '@',
+        width: '@',
+        xfbml: '@'
       },
-      template: '<h1>Hello {{name}}</h1>'
+      template: `<div>
+        <div id="fb-root"></div>
+        <div class="fb-page"
+          data-href={{href}}
+          data-width={{width}}
+          data-height={{height}}
+          data-adapt-container-width={{adaptContainerWidth}}
+          data-tabs={{tabs.join()}}
+          data-hide-cover={{hideCover}}
+          data-show-facepile={{showFacepile}}
+          data-hide-cta={{hideCta}}
+          data-small-header={{smallHeader}}
+          ></div>
+      </div>`,
+      link: (scope) => {
+        // Replace undefined values with default props
+        _.defaults(scope, {
+          adaptContainerWidth: true,
+          locale: 'en_US',
+          height: 500,
+          hideCover: false,
+          hideCta: false,
+          showFacepile: true,
+          smallHeader: false,
+          tabs: ['timeline'],
+          version: 'v3.1',
+          width: 340,
+          xfbml: true,
+        });
+
+        $window.fbAsyncInit = () => {
+          FB.init({
+            appId: scope.appId,
+            xfbml: scope.xfbml,
+            version: scope.version,
+          });
+        };
+    
+        // Load the SDK asynchronously
+        ((d, s, id) => {
+          const element = d.getElementsByTagName(s)[0];
+          const fjs = element;
+          let js = element;
+          if (d.getElementById(id)) {return;}
+          js = d.createElement(s); js.id = id;
+          js.src = `//connect.facebook.net/${scope.locale}/sdk.js`;
+          fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+      }
     }
-  }).name;
+  }]).name;
